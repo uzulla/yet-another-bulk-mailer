2d8948f7b615592417a9c319acdd6f37ef86f848
uzulla <uzulla@himitsukichi.com>
2019-01-07 16:31:53 +0900

    format

--- do.php
+++ do.php
@@ -15,25 +15,25 @@ $mail_list_raw = explode("\n", $_POST['mail_list']);
 
 $mail_list = [];
 foreach ($mail_list_raw as $email) {
-    $email=trim($email);
+    $email = trim($email);
     if (!preg_match("/@/", $email)) continue;
-    
+
     $result = preg_match("|\A(.*)\t(.*)\z|u", $email, $m);
-    if(!$result){
+    if (!$result) {
         echo "<span style='color:red'>宛先のフォーマットが不正です。（「本当に」タブでくぎっていますか？）</span>\n";
         var_dump($email);
         exit;
     }
 
     $mail_list[] = [
-        'name'=>$m[1],
-        'email'=>$m[2]
+        'name' => $m[1],
+        'email' => $m[2]
     ];
 }
 
 echo "<h3>送信先メアド</h3>";
 
-echo htmlspecialchars(print_r($mail_list,1), ENT_QUOTES);
+echo htmlspecialchars(print_r($mail_list, 1), ENT_QUOTES);
 echo "<hr>";
 echo "<h3>送信テンプレート</h3>";
 
@@ -49,7 +49,7 @@ echo htmlspecialchars("\nFROM email:\n{$from_email}", ENT_QUOTES);
 $cc_email = $_POST['cc'];
 echo htmlspecialchars("\nCC:\n{$cc_email}", ENT_QUOTES);
 $orig_cc_list = explode(',', $cc_email);
-if(count($orig_cc_list)===1 && $orig_cc_list[0] === ""){ // explode不便すぎる
+if (count($orig_cc_list) === 1 && $orig_cc_list[0] === "") { // explode不便すぎる
     $orig_cc_list = [];
 }
 
@@ -82,20 +82,20 @@ $message
 foreach ($mail_list as $mail) {
     echo "<h3>送信メール</h3>";
 
-    if(preg_match('/,/u', $mail['email'])){
+    if (preg_match('/,/u', $mail['email'])) {
         $list = explode(',', $mail['email']);
         // １つ目はtoにする
         $to = array_shift($list);
         // 二つ目以降はCCに落とす
         $cc_list = array_merge($list, $orig_cc_list);
-    }else{
+    } else {
         $to = $mail['email'];
         $cc_list = $orig_cc_list;
     }
 
-    if($mail['name'] !== "-") { // 名前が-なら、名前を設定しない
+    if ($mail['name'] !== "-") { // 名前が-なら、名前を設定しない
         $message->setTo([$to => $mail['name']]);
-    }else{
+    } else {
         $message->setTo([$to]);
     }
     $message->setCc($cc_list);
@@ -107,23 +107,23 @@ foreach ($mail_list as $mail) {
     // 画面表示用に用意(Swiftmailerから取り出す)
     $swift_address = array_keys($message->getTo())[0]; // php7にしてarray_key_firstにしたい…
     $swift_name = $message->getTo()[$swift_address];
-    
-    echo "TO: ".htmlspecialchars("{$swift_name}<{$swift_address}>\n", ENT_QUOTES);
+
+    echo "TO: " . htmlspecialchars("{$swift_name}<{$swift_address}>\n", ENT_QUOTES);
     echo "CC: ";
     var_dump(array_keys($message->getCc()));
 
-    echo htmlspecialchars("件名:". $message->getSubject()."\n", ENT_QUOTES);
+    echo htmlspecialchars("件名:" . $message->getSubject() . "\n", ENT_QUOTES);
     echo "本文:<br><pre style='background-color:#eeeeee'>";
-    echo htmlspecialchars($message->getBody()."\n", ENT_QUOTES);
+    echo htmlspecialchars($message->getBody() . "\n", ENT_QUOTES);
     echo "</pre>";
 
     // Send the message
     if ($is_no_dry_run) {
         $result = $mailer->send($message);
         echo "result:";
-        if($result>0){
+        if ($result > 0) {
             echo "成功 ({$result}通送信)";
-        }else{
+        } else {
             echo "失敗？ ({$result}通送信)";
         }
     } else {
